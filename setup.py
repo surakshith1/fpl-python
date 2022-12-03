28aad69a0b5f7ac128fb2a471e2322c795a25171
surakshith1 <surakshith.gowda@gmail.com>
2022-12-03 17:24:40 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="fpl-python",  # Replace with the name of your library
+    version="0.1.0",  # Replace with the version of your library
+    packages=["fpl_python"],  # Replace with the name of your Python package
+    include_package_data=True,
+    install_requires=["requests"],  # Replace with any required dependencies
+    author="Surakshith",  # Replace with your name
+    author_email="surakshith.gowda@email.com",  # Replace with your email address
+    description="A Python library for accessing the FPL API",  # Replace with a description of your library
+    keywords="fpl fantasy premier league api",
+    url="https://github.com/surakshith1/fpl-python",  # Replace with the URL of your library on GitHub
+)
